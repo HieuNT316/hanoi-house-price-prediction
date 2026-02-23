e75b4d1bc1e32d9c4b6563d6648951c19dca28e2
Alexus143 <optimusrose3@gmail.com>
2026-02-23 16:56:16 +0700

    update info fo postgre

--- src/config.py
+++ src/config.py
@@ -19,3 +19,12 @@ MODEL_PATH = os.path.join(DATA_DIR, 'house_price_model.pkl')
 
 # Đảm bảo thư mục data tồn tại
 os.makedirs(DATA_DIR, exist_ok=True)
+
+# Thêm config cho PostgreSQL (Thay đổi thông tin cho khớp với cài đặt của bạn)
+DB_USER = "postgres"
+DB_PASS = "1403"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "real_estate_db"
+
+POSTGRES_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
